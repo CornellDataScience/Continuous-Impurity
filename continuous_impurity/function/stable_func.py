fixed bug in matrix_activation_logistic_impurity where the bias of each transformation's gradient needed to be made smaller so that the bias didn't overtake the direction weights, yielding tiny gradients

continuous_impurity/function/stable_func.py
@@ -15,11 +15,9 @@ def sigmoid(x):
     out[where_greater_0] = out_greater_0
     out[where_less_0] = out_less_0
     return out
-    '''
-    if x >= 0:
-        z = np.exp(-x)
-        return 1.0 / (1.0 + z)
-    else:
-        z = np.exp(x)
-        return z / (1.0 + z)
-    '''
+
+'''
+represents tanh() in terms of the sigmoid
+'''
+def tanh(x):
+    return 2.0*sigmoid(2.0*x) - 1.0

continuous_impurity/main.py
@@ -10,6 +10,8 @@ from sklearn.datasets import fetch_mldata
 from model.impurity.logistic_impurity_model import LogisticImpurityModel
 from model.impurity.matrix_activation_logistic_impurity import MatrixActivationLogisticImpurity
 from function.activation.sigmoid import Sigmoid
+from function.activation.tanh import TanH
+from function.activation.identity import Identity
 #REMINDER: Use np.float_power instead of ** or np.power for fractional powers
 #TODO: make a more general framework for treebased models using continuous impurity. I.e. make
 #a more modular class that can be extended and have some abstract functions implemented to give
@@ -20,7 +22,7 @@ from function.activation.sigmoid import Sigmoid
 #TODO: Make a version of logistic trees that does not force being binary
 
 
-X,y = datasets.load_breast_cancer(return_X_y = True)#load_iris(return_X_y = True)
+X,y = datasets.load_iris(return_X_y = True)#load_iris(return_X_y = True)
 X = X.astype(np.float64)
 FEATURES = [0,1]
 X = X[:,FEATURES]
@@ -31,10 +33,12 @@ def change_basis(X):
     return X
     #return np.column_stack([X, X**2, X[:,0]*X[:,1]])
 
-y[np.where(y==2)] = 0
+y[np.where(y==2)] = 1
 #X -= np.mean(X[np.where(y==1)], axis = 0)
 #X/=np.max(np.abs(X))
-
+colors = [["blue", "red", "green"][y[i]] for i in range(y.shape[0])]
+plt.scatter(X[:,0], X[:,1], color = colors)
+plt.show()
 
 '''
 mnist = fetch_mldata('MNIST original')
@@ -45,7 +49,7 @@ y = mnist.target
 print("X max: ", X.max())
 print("X min: ", X.min())
 NUM_TRAIN = int(0.8*X.shape[0])
-np.random.seed(seed = 42)
+#np.random.seed(seed = 42)
 PERMUTE_INDS = np.random.permutation(np.arange(0, X.shape[0]))
 
 TRAIN_INDS = PERMUTE_INDS[:NUM_TRAIN]
@@ -56,8 +60,12 @@ X_test = change_basis(X[TEST_INDS])
 y_test = y[TEST_INDS]
 
 plt.show()
-model = MatrixActivationLogisticImpurity(Sigmoid(), X_train.shape[1], 5)#LogisticImpurityModel(X_train.shape[1])#
-model.train(X_train, y_train, 200000, [.01, .1], print_progress_iters = 1000)
+'''
+PROBLEM: using T(Ax) may not be nonlinearenough to represent any interesting transformation. Even though
+the idea is to nest them using a tree-like structure, often makes all the axises look practically equal (points generally lie on y = x plot)
+'''
+model = MatrixActivationLogisticImpurity(TanH(), X_train.shape[1], 2)#LogisticImpurityModel(X_train.shape[1])#
+model.train(X_train, y_train, 100000, [1,1], print_progress_iters = 1000)
 
 
 predictions = model.predict(X_test)
@@ -82,6 +90,9 @@ plt.scatter(X[:,0], X[:,1], color = colors)
 plt.show()
 
 
+X_transformed = model._get_mat_act_transform().transform(data_helper.affine_X(X))
+plt.scatter(X_transformed[:,0], X_transformed[:,1], color = colors)
+plt.show()
 
 
 

continuous_impurity/model/impurity/impurity_model.py
@@ -6,10 +6,8 @@ import timeit
 
 class ImpurityModel(ABC):
 
-    def __init__(self, num_subgroups, params_shapes):
-        self.__params = []
-        for i in range(len(params_shapes)):
-            self.__params.append(0.00001*(np.random.random(params_shapes[i])-.5).astype(np.float64))
+    def __init__(self, num_subgroups, params):
+        self.__params = params
         self.__num_subgroups = num_subgroups
 
     '''returns a matrix, A, with shape (X.shape[0], self.__num_subgroups)
@@ -39,6 +37,7 @@ class ImpurityModel(ABC):
             if iter%print_progress_iters == 0:
                 print("iter: ", iter)
                 print("expected gini: ", self.__expected_gini(X,y))
+                print("GINI: ", self.__gini(X,y))
                 print("params: ", self.__params)
                 print("grad: ", grad)
                 print("---------------------------------------------")
@@ -93,6 +92,13 @@ class ImpurityModel(ABC):
 
     def __expected_gini(self, X, y):
         return impurity.expected_gini(self.predict(X), y)
+
+    def __gini(self, X, y):
+        predicts = self.predict(X)
+        groups = np.argmax(predicts, axis = 1)
+        left = y[np.where(groups == 0)]
+        right = y[np.where(groups == 1)]
+        return impurity.gini([left, right])
 
     '''
     expects d_predicts to have shape (predicts.shape) + (theta.shape),

continuous_impurity/model/impurity/logistic_impurity_model.py
@@ -6,7 +6,7 @@ import function.stable_func as stable_func
 class LogisticImpurityModel(ImpurityModel):
 
     def __init__(self, x_length):
-        ImpurityModel.__init__(self, 2, [(x_length+1)])
+        ImpurityModel.__init__(self, 2, [0.00001*(np.random.random(x_length+1)-.5).astype(np.float64)])
 
 
 

continuous_impurity/model/impurity/matrix_activation_logistic_impurity.py
@@ -6,73 +6,56 @@ from model.parameterized_transform.matrix_activation_transform import MatrixActi
 import timeit
 
 class MatrixActivationLogisticImpurity(ImpurityModel):
-
+    TRANSFORM_BIAS_GRADIENT_DAMPEN_FACTOR = 0.00001
     def __init__(self, act_func, x_length, transform_x_length):
         self.__act_func = act_func
         mat_shape = (transform_x_length, x_length+1)
-        ImpurityModel.__init__(self, 2, [(transform_x_length), mat_shape])
+        param0 = 0.00001*(np.random.random(x_length+1)-.5).astype(np.float64)
+        param1 = 0.00001*(np.random.random(mat_shape)-.5).astype(np.float64)
+        for i in range(x_length):
+            param1[i,i] = 1
+        ImpurityModel.__init__(self, 2, [param0, param1])
 
-    def __get_mat_act_transform(self):
+    def __transform_prepare_X(self, X):
+        return data_helper.affine_X(X)
+
+    def __dot_prepare_transform(self, transformed_X):
+        return data_helper.affine_X(transformed_X)#transformed_X
+
+    def _get_mat_act_transform(self):
         return MatrixActivationTransform(self.__act_func, self._get_params()[1])
 
     '''returns a matrix, A, with shape (X.shape[0], self.__num_subgroups)
     where A[i,j] is the probability of X[i] being assigned to subset j.'''
     def predict(self, X):
-        X_affine = data_helper.affine_X(X)
-        transforms = self.__get_mat_act_transform().transform(X_affine)
-        p0 = stable_func.sigmoid(np.dot(transforms, self._get_params()[0]))
+        X_prep = self.__transform_prepare_X(X)
+        transforms = self._get_mat_act_transform().transform(X_prep)
+        transforms_prep = self.__dot_prepare_transform(transforms)
+        p0 = stable_func.sigmoid(np.dot(transforms_prep, self._get_params()[0]))
         return np.column_stack([p0, 1-p0])
 
 
     def _d_predict_d_params(self, X, predicts):
-        times = []
-
-        X_affine = data_helper.affine_X(X)
-
-        start_time = timeit.default_timer()
-        transform_X_affine = self.__get_mat_act_transform().transform(X_affine)
-        times.append(timeit.default_timer()-start_time)
-
-
-        start_time = timeit.default_timer()
+        X_prep = self.__transform_prepare_X(X)
+        transform_X_prep = self.__dot_prepare_transform(self._get_mat_act_transform().transform(X_prep))
         sigmoid_deriv = predicts[:,0]*(1.0-predicts[:,0])
-        times.append(timeit.default_timer()-start_time)
-
-
-        start_time = timeit.default_timer()
-        grad_predict0_params0 = transform_X_affine*(sigmoid_deriv[:,np.newaxis])
-        times.append(timeit.default_timer()-start_time)
-
-
-        start_time = timeit.default_timer()
-        mat_act_derivs = self.__get_mat_act_transform().param_grad(X_affine, transform_X_affine)
-        times.append(timeit.default_timer()-start_time)
+        grad_predict0_params0 = transform_X_prep*(sigmoid_deriv[:,np.newaxis])
 
 
 
         grad_predict0_params1 = np.zeros((X.shape[0],) + self._get_params()[1].shape, dtype = np.float64)
-
-        start_time = timeit.default_timer()
-        #slowest
-
+        mat_act_derivs = self._get_mat_act_transform().param_grad(X_prep, transform_X_prep)
         for j in range(grad_predict0_params1.shape[1]):
-            grad_predict0_params1[:,j] = sigmoid_deriv[:,np.newaxis]*\
-                self._get_params()[0][j]*\
-                mat_act_derivs[:,j,j]
+            grad_predict0_params1[:,j] = (sigmoid_deriv[:,np.newaxis])*self._get_params()[0][j]*mat_act_derivs[:,j,j]
+
+        grad_predict0_params1[:,grad_predict0_params1.shape[1]-1] *= MatrixActivationLogisticImpurity.TRANSFORM_BIAS_GRADIENT_DAMPEN_FACTOR
 
 
-        start_time = timeit.default_timer()
         grad_predict0_params = [grad_predict0_params0, grad_predict0_params1]
         out = []
         for i in range(2):
-            outi = np.zeros((X_affine.shape[0], 2) + self._get_params()[i].shape, dtype = np.float64)
+            outi = np.zeros((X_prep.shape[0], 2) + self._get_params()[i].shape, dtype = np.float64)
             outi[:,0] = grad_predict0_params[i]
             outi[:,1] = -grad_predict0_params[i]
             out.append(outi)
-        times.append(timeit.default_timer()-start_time)
-
-        times = np.asarray(times)
-        #print("times: ", times)
-        #print("relative times: ", times/np.sum(times))
-        #print("--------------------------------------------------------------------------")
         return out