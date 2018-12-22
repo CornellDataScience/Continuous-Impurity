fixed bug in global_impurity_tree4 that made calculating p gradients O(nodes^2) instead of O(nodes)

continuous_impurity/global_impurity4_testing.py
@@ -23,11 +23,11 @@ def d_sigmoid(X, sigmoid_outs):
     return sigmoid_outs*(1-sigmoid_outs)
 
 np.random.seed(seed = 42)
-X,y = datasets.load_iris(return_X_y = True)#datasets.load_digits(return_X_y = True)#
+X,y = datasets.load_digits(return_X_y = True)#datasets.load_iris(return_X_y = True)#
 FEATURES = range(X.shape[1])#[0,1]#
 X = X[:, FEATURES]
 X = X.astype(D_TYPE)
-#X/=16.0
+X/=16.0
 NUM_POINTS = X.shape[0]
 
 X = X[0:NUM_POINTS,:]
@@ -51,26 +51,10 @@ TODO: Make sure that transition from accidentally not treating leaves as leaves,
 
 
 start_time = timeit.default_timer()
-splits = tree.calc_split_tree(X)
-#print("splits: ", splits.shape)
-#print("SPLITS: ", splits)
 
-grad_splits = tree.calc_grad_split_tree(X, splits)
-#print("grad_splits: ", grad_splits.shape)
-#print("GRAD_SPLITS: ", grad_splits)
+tree.train(X,y,50000,50)
 
-p_leaves = tree.calc_p_leaves(splits, slow_assert = False)
-#print("p_leaves: ", p_leaves.shape)
-#print("P_LEAVES: ", p_leaves)
-
-grad_p_leaves = tree.calc_grad_p_leaves(splits, grad_splits, p_leaves)
-
-grad_gini = tree.calc_expected_gini_gradient(X, where_y_eq_ls)
-print("grad_gini: ", grad_gini)
-
-tree.train(X,y,50000,1)
-
-print("p_leaves calc'd in: ", timeit.default_timer() - start_time)
+print("model trained in: ", timeit.default_timer() - start_time)
 '''
 grad_p_leaves_label_sums = tree.calc_label_sum_grad_p_leaves(p_leaves, splits, grad_splits, where_y_eq_ls)
 #print("grad_p_leaves_label_sums: ", grad_p_leaves_label_sums.shape)

continuous_impurity/model/impurity/global_impurity4/global_impurity_tree4.py
@@ -3,7 +3,7 @@ import function.stable_func as stable_func
 import function.impurity as impurity
 import model.impurity.global_impurity4.array_binary_tree as arr_tree
 import toolbox.numpy_helper as numpy_helper
-
+from performance.stopwatch_profiler import StopwatchProfiler
 
 class GlobalImpurityTree4:
 
@@ -14,6 +14,7 @@ class GlobalImpurityTree4:
         self.__d_split_func = d_split_func
         self.__params_depth = arr_tree.depth_from(self.__params_tree, arr_tree.root())
         self.__leaf_root_paths = self.__calc_leaf_paths_to_root(self.__params_tree)
+        self.__where_rights = self.__calc_where_rights()
 
 
 
@@ -24,6 +25,19 @@ class GlobalImpurityTree4:
         for d_from_leaf in range(1, self.__params_depth + 1):
             out[:,out.shape[1] - 1 - d_from_leaf] = arr_tree.parent(out[:,out.shape[1] - d_from_leaf])
         return out.astype(np.int)
+
+    #returns a list A s.t. A[d] is the numpy array of all nodes at that depth
+    def __calc_where_rights(self):
+        out = []
+        for d in range(self.__params_depth):
+            qs = self.__leaf_root_paths[:,d]
+            q_cs = self.__leaf_root_paths[:,d+1]
+            q_c_child_nums = arr_tree.child_num(qs, q_cs)
+
+            #pretty simple pattern here if can think of a fast way to do that instead
+            #of using np.where() (print q_c_child_nums to see)
+            out.append(np.where(q_c_child_nums == 1))
+        return out
 
     def calc_split_tree(self, X):
         return self.__split_func(np.dot(self.__params_tree, X.T))
@@ -71,31 +85,31 @@ class GlobalImpurityTree4:
         leaf_bounds = arr_tree.node_at_depth_range(self.__params_depth + 1)
         return p_tree[leaf_bounds[0]:leaf_bounds[1]]
 
+
+
     def calc_grad_p_leaves(self, split_tree, grad_split_tree, p_leaves, slow_assert = True):
+        #For further improvement: Need to be careful when using by-entire-depth "speedups",
+        #the last one I did actually did O(nodes^2) calculations since a lot of slice of
+        #self.__leaf_root_paths[:,d] were repeated at higher depths!
+
         #indexed by (k is leaf, q is a reachable node from k)
         #out[k, depth(q), i] = grad p(k|X[i]) w.r.t. theta_q
         out = np.zeros((p_leaves.shape[0], self.__params_depth) + grad_split_tree.shape[1:], dtype = np.float32)
-        for d in range(self.__params_depth):
-            qs = self.__leaf_root_paths[:,d]
-            q_cs = self.__leaf_root_paths[:,d+1]
-            q_c_child_nums = arr_tree.child_num(qs, q_cs)
 
-            #pretty simple pattern here if can think of a fast way to do that instead
-            #of using np.where() (print q_c_child_nums to see)
-            where_rights = np.where(q_c_child_nums == 1)
-            d_splits = split_tree[qs].copy()
-            d_splits[where_rights] = 1-d_splits[where_rights]
+        for k in range(out.shape[0]):
+            for d in range(self.__leaf_root_paths.shape[1] - 1):
+                q = self.__leaf_root_paths[k,d]
+                q_c = self.__leaf_root_paths[k,d+1]
+                if arr_tree.child_num(q, q_c) == 0:
+                    d_split = split_tree[q]
+                    d_grad_split = grad_split_tree[q]
+                else:
+                    d_split = 1.0-split_tree[q]
+                    d_grad_split = -grad_split_tree[q]
 
-            d_grad_splits = grad_split_tree[qs].copy()
-            d_grad_splits[where_rights] = -d_grad_splits[where_rights]
-
-            #not sure what value to use for the div_0_val
-            out[:,d] = numpy_helper.stable_divide(p_leaves, d_splits, 0)[:,:,np.newaxis] *\
-                d_grad_splits
-        if slow_assert:
-            #TODO
-            return out
+                out[k,d] = numpy_helper.stable_divide(p_leaves[k], d_split, 0)[:,np.newaxis] * d_grad_split
         return out
+
 
     def train(self, X, y, n_iters, learn_rate):
         unique_labels = np.unique(y)
@@ -107,7 +121,7 @@ class GlobalImpurityTree4:
 
             grad_gini = self.calc_expected_gini_gradient(X, where_y_eq_ls)#self.calc_expected_gini_gradient_slow(p_leaves, grad_p_leaves, y, unique_labels)
             self.__params_tree -= learn_rate*grad_gini
-            if iter%50 == 0:
+            if iter%10 == 0:
                 print("Iter: ", iter)
                 split_tree = self.calc_split_tree(X)
                 p_leaves = self.calc_p_leaves(split_tree)
@@ -116,12 +130,21 @@ class GlobalImpurityTree4:
 
 
     def calc_expected_gini_gradient(self, X, where_y_eq_ls, slow_assert = True):
+        stopwatch = StopwatchProfiler()
+        stopwatch.start()
+
         leaf_range = arr_tree.node_at_depth_range(self.__params_depth+1)
         splits = self.calc_split_tree(X)
-        grad_splits = self.calc_grad_split_tree(X, splits, slow_assert = False)
-        p_leaves = self.calc_p_leaves(splits, slow_assert = False)
-        grad_p_leaves = self.calc_grad_p_leaves(splits, grad_splits, p_leaves, slow_assert = False)
+        stopwatch.lap("spilts calc'd")
 
+        grad_splits = self.calc_grad_split_tree(X, splits, slow_assert = False)
+        stopwatch.lap("grad_splits calc'd")
+
+        p_leaves = self.calc_p_leaves(splits, slow_assert = False)
+        stopwatch.lap("p_leaves calc'd")
+
+        grad_p_leaves = self.calc_grad_p_leaves(splits, grad_splits, p_leaves, slow_assert = False)
+        stopwatch.lap("grad_p_leaves calc'd")
 
         out = np.zeros(self.__params_tree.shape, dtype = self.__params_tree.dtype)
 
@@ -135,7 +158,7 @@ class GlobalImpurityTree4:
 
         p_leaf_sums = np.sum(label_p_leaf_sums, axis = 0)
         grad_p_leaf_sums = np.sum(label_grad_p_leaf_sums, axis = 0)
-
+        stopwatch.lap("sums calc'd")
 
         u = self.__calc_u(p_leaf_sums)
         v = self.__calc_v(label_p_leaf_sums)
@@ -147,9 +170,11 @@ class GlobalImpurityTree4:
             nodes_at_q_d = self.__leaf_root_paths[:,q_d]
             unq_nodes_at_q_d = np.unique(nodes_at_q_d)
 
+            #grad_summands = u[:,np.newaxis]*grad_v_wrt_q + v[:,np.newaxis]*grad_u_wrt_q
+
             #WORKS
             for k in range(0, leaf_range[1]-leaf_range[0]):
-                out[nodes_at_q_d[k]] += u[k]*grad_v_wrt_q[k] + v[k]*grad_u_wrt_q[k]
+                out[nodes_at_q_d[k]] += u[k]*grad_v_wrt_q[k] + v[k]*grad_u_wrt_q[k]#grad_summands[k]#
 
 
             #DOESN'T WORK (because nodes_at_q_d often contains duplicates and it seems to only do one assign
@@ -159,6 +184,11 @@ class GlobalImpurityTree4:
             #out[nodes_at_q_d] += u[:,np.newaxis]*grad_v_wrt_q + v[:,np.newaxis]*grad_u_wrt_q'''
 
         out *= -1.0/float(p_leaves.shape[1])
+        stopwatch.lap("out calc'd")
+        stopwatch.stop()
+        print("relative laps; ", stopwatch.relative_lap_deltas())
+
+        stopwatch.reset()
         if slow_assert:
             #TODO
             return out